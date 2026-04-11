7ae83ebe4fbc0c384671616f3eff927078dc330b
Kartik Patel <144128354+kartik941dev@users.noreply.github.com>
2026-04-11 12:12:06 +0000

    geteer setter

---
+++ 10_classes_oops/gettersetters.js
@@ -0,0 +1,28 @@
+// it is class based accessing get set values 
+// many times / mostly we will use this 
+
+
+class User {
+    constructor(email, password){
+        this.email = email;
+        this.password = password
+    }
+
+    get email(){
+        return this._email.toUpperCase()
+    }
+    set email(value){
+        this._email = value
+    }
+
+    get password(){
+        return `${this._password}hitesh`
+    }
+
+    set password(value){
+        this._password = value
+    }
+}
+
+const hitesh = new User("h@hitesh.ai", "abc")
+console.log(hitesh.email);
